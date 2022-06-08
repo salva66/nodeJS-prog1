5049e65fb56c0f2eccd9f3823c1548a2f82ded93
k_rama197 <mrkarama2@gmail.com>
2022-06-09 00:48:04 +0300

    Update hello.js

--- LAB1/hello.js
+++ LAB1/hello.js
@@ -1,17 +1,13 @@
 const { fstat } = require("fs");
 const http = require("http");
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
 
 //port of the server
 const host = 'localhost';
 const port = 8000;
 const someObject = require('./stud_array.json');
-global.document = new JSDOM(html).window.document;
-const myJson =JSON.stringify(someObject);
-window.document.getElementsByName.innerHTML = myJson;
+
 const requestListener = function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'}); //sets HTTP status code of the response
+    res.writeHead(200); //sets HTTP status code of the response
     
     res.end();
 };
